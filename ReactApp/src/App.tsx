7489146e8330642a0d3d1c04370a5d3512f3718a
Erlend <erlend@tyrmi.com>
2021-09-14 11:31:42 +0200

    Added Material UI

--- ReactApp/src/App.tsx
+++ ReactApp/src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TextField id="filled-basic" label="Filled" variant="filled" /><br/>
+      <Button variant="contained" color="primary">
+        Commit
+      </Button>
     </div>
   );
 }
