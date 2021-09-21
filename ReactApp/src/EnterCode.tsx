d469f2a494e42b846005426b51657e1a6f60347f
Erlend <erlend@tyrmi.com>
2021-09-21 15:01:31 +0200

    entercode better redirect button

--- ReactApp/src/EnterCode.tsx
+++ ReactApp/src/EnterCode.tsx
@@ -1,9 +1,19 @@
 import React, {Component} from 'react';
-import {Box, TextField, Typography} from "@mui-material";
-
+import {Box, TextField, Typography, Button, Link} from "@mui/material";
+import {Redirect} from 'react-router-dom'
+import { useHistory } from 'react-router-dom';
 
 export default class EnterCode extends Component {
+
+    // Set initial state
+    state = {
+        checkedIn: false
+    }
+
     render() {
+        if (this.state.checkedIn) {
+            return <Redirect to='/Kvis'/>
+        }
         return (
             <Box m="auto" width={400}>
                 <Typography align={"center"}>ENTER CODE</Typography>
@@ -17,7 +27,20 @@ export default class EnterCode extends Component {
                     autoComplete="code123"
                     autoFocus
                 />
+                <StartQuizComponent />
             </Box>
         );
     }
 }
+
+const StartQuizComponent = () => {
+    const history = useHistory();
+    const handleClick = () => history.push('/kvis');
+
+    return (
+        <Button type="button" onClick={handleClick}>
+            Navigate to another route
+        </Button>
+    );
+};
+
