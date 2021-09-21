f4571d65184b37a3958779e568583da3488913cd
Erlend <erlend@tyrmi.com>
2021-09-21 13:19:08 +0200

    started header and entercode comps

--- ReactApp/src/EnterCode.tsx
+++ ReactApp/src/EnterCode.tsx
@@ -1,9 +1,23 @@
 import React, {Component} from 'react';
+import {Box, TextField, Typography} from "@mui-material";
+
 
 export default class EnterCode extends Component {
     render() {
         return (
-            <h1>ENTER CODE</h1>
+            <Box m="auto" width={400}>
+                <Typography align={"center"}>ENTER CODE</Typography>
+                <TextField
+                    margin="normal"
+                    required
+                    id="code"
+                    label="Kvis Code"
+                    fullWidth
+                    name="code"
+                    autoComplete="code123"
+                    autoFocus
+                />
+            </Box>
         );
     }
 }
--- ReactApp/src/Header.tsx
+++ ReactApp/src/Header.tsx
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
+import {Paper, Typography} from "@material-ui/core"
+import Grid from '@material-ui/core/Grid';
+import logo from './ImageAssets/kvislogo.png';
+import Box from '@material-ui/core/Box';
 
-var questionNumber = 1
+
+var questionNumber = 53
 
 export default class Header extends Component {
     render() {
-        return (<div>
-            <h1>LOGO</h1>
-            <p>Question {questionNumber}</p>
-        </div>);
+        let questionNumberDisplay = questionNumber === 0 ? "" :
+            <Typography variant="subtitle1" align={"center"}>Question {questionNumber}</Typography>
+
+        return (<Paper>
+            <Grid container spacing={2}>
+                <Grid item xs={4}> <img  src={logo} width="64.dp" alt="Logo"/> </Grid>
+                <Grid item xs={4} > <Box pt={3}>{questionNumberDisplay}</Box>     </Grid>
+            </Grid>
+        </Paper>);
     }
 }
