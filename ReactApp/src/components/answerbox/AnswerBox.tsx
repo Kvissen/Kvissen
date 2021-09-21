4f864f4365416d96081a195fb8c20d4bf9b04d89
Logstor <a.rottger_rydahl@live.dk>
2021-09-21 12:14:58 +0200

    Created Component and render()

---
+++ ReactApp/src/components/answerbox/AnswerBox.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class AnswerBox extends React.Component
+{
+    render()
+    {
+        return (
+            <div><h1>AnswerBox</h1></div>
+        );
+    }
+}
+
+export default AnswerBox;
