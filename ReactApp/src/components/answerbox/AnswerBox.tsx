4a99bdafe458e9b82e3484f55a526534933f3f95
Logstor <a.rottger_rydahl@live.dk>
2021-09-21 13:04:10 +0200

    Added simple style to AnswerBox Component

--- ReactApp/src/components/answerbox/AnswerBox.tsx
+++ ReactApp/src/components/answerbox/AnswerBox.tsx
@@ -1,13 +1,22 @@
 import React from "react";
+import {Paper} from "@mui/material";
 
-class AnswerBox extends React.Component
-{
-    render()
-    {
-        return (
-            <div><h1>AnswerBox</h1></div>
-        );
-    }
+type AnswerOption = {
+    text: string,
+    number: number
 }
 
-export default AnswerBox;
+export default function AnswerBox(options: AnswerOption)
+{
+    const mStyle = {
+        backgroundColor: "grey",
+        color: "blue"
+    }
+
+    return (
+        <Paper style={mStyle} elevation={5} variant={"elevation"}>
+            <h2>{options.number}</h2>
+            <p>{options.text}</p>
+        </Paper>
+    );
+}
