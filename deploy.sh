c1bbc2679b8171e95e0b9a61139f23eb4f8656ba
nikolajwassmann <s185082@dtu.dk>
2021-10-05 12:08:45 +0200

    Script now removes old directory, removes docker container and starts everything up

--- deploy.sh
+++ deploy.sh
@@ -5,19 +5,25 @@
 HOST="130.225.170.170"
 PORT="22022"
 USER="s160107"
-DIRECTORY="~/devops"
+WEBAPPDIRECTORY="~/devops/src/main/webapp"
+BASEDIRECTORY="~/devops"
 DOCKERIMAGE="kvissen"
 DOCKERPORT="8080"
 
+ssh -t -p $PORT -i id_rsa -o "StrictHostKeyChecking=no" $USER@$HOST "rm -rf $BASEDIRECTORY ; mkdir -p $WEBAPPDIRECTORY"
 
 # Mark file as readable
 chmod 400 id_rsa
 
 # Transfer webapp project
-scp -v -r -P $PORT -i id_rsa src/main/webapp/* $USER@$HOST:$DIRECTORY
+scp -v -r -P $PORT -i id_rsa src/main/webapp/* $USER@$HOST:$WEBAPPDIRECTORY
 
 # Transfer Jar file
-scp -v -r -P $PORT -i id_rsa target/Kvissen.jar $USER@$HOST:$DIRECTORY
+scp -v -r -P $PORT -i id_rsa target/Kvissen.jar $USER@$HOST:$BASEDIRECTORY
+
+# Transfer Dockerfile
+scp -v -r -P $PORT -i id_rsa Dockerfile $USER@$HOST:$BASEDIRECTORY
+
 
 # SSH to VM, Docker build and run
-ssh -t -p $PORT -i id_rsa -o "StrictHostKeyChecking=no" $USER@$HOST "cd $DIRECTORY; sudo docker build -t $DOCKERIMAGE .; sudo docker run -p $DOCKERPORT:$DOCKERPORT -d $DOCKERIMAGE"
+ssh -t -p $PORT -i id_rsa -o "StrictHostKeyChecking=no" $USER@$HOST "cd $BASEDIRECTORY; sudo docker build -t $DOCKERIMAGE .; sudo docker container stop $DOCKERIMAGE; sudo docker container rm $DOCKERIMAGE; sudo docker run --name $DOCKERIMAGE -p $DOCKERPORT:$DOCKERPORT -d $DOCKERIMAGE"
