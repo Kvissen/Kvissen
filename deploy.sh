7179aca741cafd1d17027ba80afbc2ae607135d4
nikolajwassmann <s185082@dtu.dk>
2021-10-05 11:46:54 +0200

    Deploy script builds and runs docker Added script to release pipeline

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,18 @@ HOST="130.225.170.170"
 PORT="22022"
 USER="s160107"
 DIRECTORY="~/temp"
+DOCKERIMAGE="kvissen"
+DOCKERPORT="8080"
+
 
 # Mark file as readable
 chmod 400 id_rsa
-
-# ssh to VM
-#ssh -p $PORT -i id_rsa -o "StrictHostKeyChecking=no" $USER@$HOST
 
 # Transfer webapp project
 scp -v -r -P $PORT -i id_rsa src/main/webapp/* $USER@$HOST:$DIRECTORY
 
 # Transfer Jar file
 scp -v -r -P $PORT -i id_rsa target/Kvissen.jar $USER@$HOST:$DIRECTORY
+
+# SSH to VM, Docker build and run
+ssh -t -p $PORT -i id_rsa -o "StrictHostKeyChecking=no" $USER@$HOST "cd $DIRECTORY; sudo docker build -t $DOCKERIMAGE .; sudo docker run -p $DOCKERPORT:$DOCKERPORT -d $DOCKERIMAGE"
