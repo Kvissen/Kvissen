007eb4c6f7938f6adae8b657ba323d5adddef17c
niko5297 <s185082@student.dtu.dk>
2021-10-20 20:57:13 +0200

    Added question and answer creation boxes

--- ReactApp/src/components/createKvis/CreateKvisBox.tsx
+++ ReactApp/src/components/createKvis/CreateKvisBox.tsx
@@ -1,6 +1,7 @@
-import {Component} from "react";
-import {Card, Button} from "@mui/material";
+import React, {Component} from "react";
+import {Card, Button, TextField, Grid} from "@mui/material";
 import './CreateKvisStyleSheet.css'
+import {store} from "../../stores/QuizStore";
 
 export default class CreateKvisBox extends Component {
 
@@ -8,6 +9,49 @@ export default class CreateKvisBox extends Component {
         return (
             <Card className="create-kvis-box">
                 <h2 className="create-kvis-question-header">Question 1</h2>
+                <TextField
+                    margin="normal"
+                    required
+                    label="Enter question"
+                    fullWidth
+                    autoFocus
+                />
+                <Grid direction='row' container spacing={1}>
+                    <Grid container item sm={6}>
+                        <TextField
+                            margin="normal"
+                            required
+                            label="Enter Answer 1"
+                            fullWidth
+                            autoFocus
+                        />
+                    </Grid>
+                    <Grid container item sm={6}>
+                        <TextField
+                            margin="normal"
+                            required
+                            label="Enter Answer 2"
+                            fullWidth
+                            autoFocus
+                        />
+                    </Grid>
+                    <Grid container item sm={6}>
+                        <TextField
+                            required
+                            label="Enter Answer 3"
+                            fullWidth
+                            autoFocus
+                        />
+                    </Grid>
+                    <Grid container item sm={6}>
+                        <TextField
+                            required
+                            label="Enter Answer 4"
+                            fullWidth
+                            autoFocus
+                        />
+                    </Grid>
+                </Grid>
             </Card>
         )
     }
