b9a5892be2c2d30aa83f1247b1be1232b09ef8ee
niko5297 <s185082@student.dtu.dk>
2021-10-30 12:40:52 +0200

    Added map function to dynamicly change number of kvisses Added type definition to CreateKvisBox.tsx

--- ReactApp/src/components/kvisList/KvisList.tsx
+++ ReactApp/src/components/kvisList/KvisList.tsx
@@ -1,24 +1,52 @@
 import {Grid} from "@mui/material";
 import {observer} from "mobx-react";
 import KvisListElement from "./KvisListElement";
+import {Kvis} from "../../models/Kvis";
+import {v4 as uuidv4} from 'uuid';
+import {Question} from "../../models/Question";
+import {Answer} from "../../models/Answer";
 
 export function KvisList() {
 
+    const kvisses = [
+        new Kvis(uuidv4(), "Test Kvis", [
+            new Question(uuidv4(), [
+                new Answer("Test answer", false),
+                new Answer("Test answer1", false),
+                new Answer("Test answer2", false),
+                new Answer("Test answer3", false),
+            ], "Test question")
+        ]),
+        new Kvis(uuidv4(), "Test Kvis1", [
+            new Question(uuidv4(), [
+                new Answer("Test answer", false),
+                new Answer("Test answer1", false),
+                new Answer("Test answer2", false),
+                new Answer("Test answer3", false),
+            ], "Test question1")
+        ]),
+        new Kvis(uuidv4(), "Test Kvis2", [
+            new Question(uuidv4(), [
+                new Answer("Test answer", false),
+                new Answer("Test answer1", false),
+                new Answer("Test answer2", false),
+                new Answer("Test answer3", false),
+            ], "Test question2")
+        ])
+    ]
+
     return (
         <div className="margin-container">
-            <Grid direction='row' container spacing={4} justifyContent={"center"}>
-                <Grid item sm={6}>
-                    <KvisListElement/>
-                </Grid>
-                <Grid item sm={6}>
-                    <KvisListElement/>
-                </Grid>
-                <Grid item sm={6}>
-                    <KvisListElement/>
-                </Grid>
-                <Grid item sm={6}>
-                    <KvisListElement/>
-                </Grid>
+            <Grid direction='row' container spacing={4} justifyContent={"space-between"} alignItems={"center"}>
+                {
+                    kvisses.map((kvis) => {
+                        return (
+                            <Grid item sm={6}>
+                                <KvisListElement kvis={kvis}/>
+                            </Grid>
+                        )
+                    })
+                }
             </Grid>
         </div>
     )
--- ReactApp/src/components/kvisList/KvisListElement.tsx
+++ ReactApp/src/components/kvisList/KvisListElement.tsx
@@ -2,8 +2,9 @@ import {Box, Button, Card} from "@mui/material";
 import React from "react";
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import EditIcon from '@mui/icons-material/Edit';
+import {Kvis} from "../../models/Kvis";
 
-export default function KvisListElement() {
+export default function KvisListElement({kvis} : {kvis: Kvis}) {
 
     return (
         <Box>
@@ -12,11 +13,11 @@ export default function KvisListElement() {
                     <Box p={2} display={"flex"} flexDirection={"column"} justifyContent={"space-between"}>
                         <div>
                         <h3 style={{margin: 0}}>Kvis name</h3>
-                        <p >Kvissen</p>
+                        <p >{kvis.name}</p>
                         </div>
                         <div>
                         <h3 >Questions</h3>
-                        <p style={{margin: 0}}>24 Questions</p>
+                        <p style={{margin: 0}}>{kvis.questions?.length} Questions</p>
                         </div>
                     </Box>
                     <Box p={2} display={"flex"} flexDirection={"column"} justifyContent={"space-between"} >
