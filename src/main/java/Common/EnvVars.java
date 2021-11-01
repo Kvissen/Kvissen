4dc27d8717d36ff99fa13637e7f67625833d3e03
Erlend Tyrmi <erlend@tyrmi.com>
2021-11-02 00:47:26 +0100

    moved / to start of path in prodvars, java
    Signed-off-by: Erlend Tyrmi <erlend@tyrmi.com>

--- src/main/java/Common/EnvVars.java
+++ src/main/java/Common/EnvVars.java
@@ -12,7 +12,7 @@ public class EnvVars {
     public static final String JWT_SECRET_KEY = "AJi4QfGWy6qGEMy99wLPUmcj261ck8pPFVwpiyFasnAlW05wbVXHdWHzrePox1CvBxh5gXf3hA";
 
     // Server base url
-    public static final String BASE_URL = IS_PRODUCTION ? "https://kvissen.herokuapp.com" : "http://localhost:8080/";
+    public static final String BASE_URL = IS_PRODUCTION ? "https://kvissen.herokuapp.com" : "http://localhost:8080";
     public static final String CLIENT_BASE_URL = BASE_URL + "#login-recipient"; // Note: The "/" is omitted after "#"!
 
     // Default token settings
@@ -23,5 +23,5 @@ public class EnvVars {
     public static final String TICKET_ENDPOINT = "https://auth.dtu.dk/dtu/";
     public static final String TICKET_VALIDATION_ENDPOINT = "https://auth.dtu.dk/dtu/validate";
     // Where we would like to receive ticket :-)
-    public static final String TICKET_RESPONSE_ENDPOINT = "api/auth/redirect";
+    public static final String TICKET_RESPONSE_ENDPOINT = "/api/auth/redirect";
 }
