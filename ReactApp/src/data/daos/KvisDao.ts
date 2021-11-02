904e5b4b33a2fa0d13069433cfd68f732e89ba4c
nikolajwassmann <s185082@dtu.dk>
2021-11-02 12:23:54 +0100

    Implemented rest of DAO methods for now

--- ReactApp/src/data/daos/KvisDao.ts
+++ ReactApp/src/data/daos/KvisDao.ts
@@ -34,7 +34,10 @@ class KvisDao implements IKvisDao{
     }
 
     async deleteKvis(id: string): Promise<boolean> {
-        return Promise.resolve(false);
+        return await this.httpClient.request({
+            method: 'DELETE',
+            url: 'http://localhost:7777/kvis/' + id
+        })
     }
 
     async getKvisses(): Promise<Kvis[]> {
@@ -46,15 +49,34 @@ class KvisDao implements IKvisDao{
         if (response?.kvisses) {
             return response.kvisses
         }
+        // Empty array if no kvisses were found
         return Promise.resolve([]);
     }
 
     async getKvissesForUser(userId: string): Promise<Kvis[]> {
+        const response = await this.httpClient.request({
+            method: 'GET',
+            url: 'http://localhost:7777/kvisses/' + userId
+        })
+
+        if (response?.kvisses) {
+            return response.kvisses
+        }
+        // Empty array if no kvisses were found
         return Promise.resolve([]);
     }
 
     async updateKvis(id: string, newKvis: Kvis): Promise<Kvis> {
-        return Promise.resolve(new Kvis(""));
+        return await this.httpClient.request({
+            method: 'PUT',
+            url: 'http://localhost:7777/kvis/' + id,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: {
+                newKvis,
+            }
+        })
     }
 
 }
