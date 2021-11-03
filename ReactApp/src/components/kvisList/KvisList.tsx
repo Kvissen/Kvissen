61243d9d3de36359c7b66c4ee6f5a73a208b121d
nikolajwassmann <s185082@dtu.dk>
2021-11-03 17:15:28 +0100

    You can now fetch a list of kvisses

--- ReactApp/src/components/kvisList/KvisList.tsx
+++ ReactApp/src/components/kvisList/KvisList.tsx
@@ -5,7 +5,7 @@ import {Kvis} from "../../models/Kvis";
 import {v4 as uuidv4} from 'uuid';
 import {Question} from "../../models/Question";
 import {Answer} from "../../models/Answer";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {KvisRepository} from "../../data/repositories/KvisRepository";
 
 export function KvisList() {
@@ -13,51 +13,22 @@ export function KvisList() {
 
     useEffect(() => {
         test();
-        //console.log("Result ", t)
     },[])
 
     async function test() {
-        console.log("Testing")
         await fetch("http://localhost:8080/api/kvis/all", { method: 'GET'})
             .then(res => {
             return res.json();
         })
             .then(data => {
-                const t = data as Kvis[]
-                console.log("log ", t)
-                return data as Kvis[]
+                setKvisses(data as Kvis[])
             })
 
-        //console.log("Running repo")
-        //return await KvisRepository.getInstance().getKvisses();
-    }
 
-    const kvisses = [
-        new Kvis(uuidv4(), "Test Kvis", [
-            new Question(uuidv4(), [
-                new Answer("Test answer", false),
-                new Answer("Test answer1", false),
-                new Answer("Test answer2", false),
-                new Answer("Test answer3", false),
-            ], "Test question")
-        ]),
-        new Kvis(uuidv4(), "Test Kvis1", [
-            new Question(uuidv4(), [
-                new Answer("Test answer", false),
-                new Answer("Test answer1", false),
-                new Answer("Test answer2", false),
-                new Answer("Test answer3", false),
-            ], "Test question1")
-        ]),
-        new Kvis(uuidv4(), "Test Kvis2", [
-            new Question(uuidv4(), [
-                new Answer("Test answer", false),
-                new Answer("Test answer1", false),
-                new Answer("Test answer2", false),
-                new Answer("Test answer3", false),
-            ], "Test question2")
-        ])
-    ]
+
+
+    }
+    const [kvisses, setKvisses] = useState<Kvis[]>([]);
 
     return (
         <div className="margin-container">
@@ -77,4 +48,4 @@ export function KvisList() {
 }
 
 const KvisListObserver = observer(KvisList)
-export default KvisListObserver
+export default KvisListObserver;
