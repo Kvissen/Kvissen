1fea3b7acfc867a52f77b98517ac7308cba4db9e
niko5297 <s185082@student.dtu.dk>
2021-11-08 18:39:03 +0100

    Added and implemented User API DAO

--- ReactApp/src/data/daos/UserDao.ts
+++ ReactApp/src/data/daos/UserDao.ts
@@ -1,25 +1,61 @@
 import {IUserDao} from "./IUserDao";
 import {User} from "../../models/User";
+import HttpClient from "../infrastructure/HttpClient";
+import {Kvis} from "../../models/Kvis";
 
-export class UserDao implements IUserDao{
-    createUser(user: User): Promise<boolean> {
+class UserDao implements IUserDao{
+
+    private static _instance: UserDao
+
+    private httpClient: HttpClient
+
+    private constructor() {
+        this.httpClient = new HttpClient();
+    }
+
+    public static getInstance(): UserDao{
+        if (this._instance == null) {
+            this._instance = new UserDao();
+        }
+        return this._instance;
+    }
+
+
+    async createUser(user: User): Promise<boolean> {
+        const url = process.env.REACT_APP_BASE_URL! + process.env.REACT_APP_API_CREATE_USER
+        return await this.httpClient.request({
+            method: 'POST',
+            url: url,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: user
+        })
+    }
+
+    async deleteUser(id: string): Promise<boolean> {
+        //TODO: Implement me
         return Promise.resolve(false);
     }
 
-    deleteUser(id: string): Promise<boolean> {
-        return Promise.resolve(false);
+    async getUserById(userId: string): Promise<User> {
+        //TODO: Implement me
+        return Promise.resolve(new User());
     }
 
-    getUserById(userId: string): Promise<User> {
-        return Promise.resolve(undefined);
+    async getUsers(): Promise<User[]> {
+        const url = process.env.REACT_APP_BASE_URL! + process.env.REACT_APP_API_GET_USERS
+        return await this.httpClient.request({
+            method: 'GET',
+            url: url
+        }).then(data => {
+            return data as User[];
+        });
     }
 
-    getUsers(): Promise<User[]> {
-        return Promise.resolve([]);
-    }
-
-    updateUser(id: string, newUser: User): Promise<User> {
-        return Promise.resolve(undefined);
+    async updateUser(id: string, newUser: User): Promise<User> {
+        //TODO: Implement me
+        return Promise.resolve(new User());
     }
 
 }
