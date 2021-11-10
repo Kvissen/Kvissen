d9832993e5bc892a098e871d3a74d1825df720fb
niko5297 <s185082@student.dtu.dk>
2021-11-10 14:19:56 +0100

    Added tests for KvisList and KvisListElement

--- ReactApp/src/components/kvisList/KvisList.test.tsx
+++ ReactApp/src/components/kvisList/KvisList.test.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import {act, render, screen} from "@testing-library/react";
+import KvisListObserver from "./KvisList";
 
-it("should", () => {
+beforeEach(() => {
+    act(() => {
+        render(<KvisListObserver/>)
+    })
+})
 
+it("should contain HTML elements", () => {
+    const container = screen.getByTestId("kvislist-test-container");
+
+    expect(container).toBeTruthy();
 })
--- ReactApp/src/components/kvisList/KvisListElement.test.tsx
+++ ReactApp/src/components/kvisList/KvisListElement.test.tsx
@@ -1,5 +1,65 @@
 import React from "react";
+import {act, render, screen} from "@testing-library/react";
+import KvisListElement from "./KvisListElement";
+import {Kvis} from "../../models/Kvis";
+import {Question} from "../../models/Question";
+import {Answer} from "../../models/Answer";
 
-it("should", () => {
+let mockAnswers = [
+    new Answer("testAnswer"),
+    new Answer("testAnswer"),
+    new Answer("testAnswer"),
+    new Answer("testAnswer"),
+];
 
+let mockQuestions = [
+    new Question(mockAnswers,"testQuestion")
+];
+
+let mockKvis = new Kvis(
+    "testId",
+    "testName",
+    "testCreator",
+    0,
+    mockQuestions
+);
+
+beforeEach(() => {
+    act(() => {
+        render(<KvisListElement kvis={mockKvis}/>)
+    })
 })
+
+it("should contain HTML elements", () => {
+    const container = screen.getByTestId("kvislistelement-test-container");
+    const kvisHeader = screen.getByTestId("kvislistelement-test-kvisname-header");
+    const questionHeader = screen.getByTestId("kvislistelement-test-questions-header");
+    const playButton = screen.getByTestId("kvislistelement-test-play");
+    const editButton = screen.getByTestId("kvislistelement-test-edit");
+
+    expect(container).toBeTruthy();
+
+    expect(kvisHeader).toBeTruthy();
+    expect(kvisHeader.textContent).toBe("Kvis name")
+
+    expect(questionHeader).toBeTruthy();
+    expect(questionHeader.textContent).toBe("Questions")
+
+    expect(playButton).toBeTruthy();
+    expect(playButton.textContent).toBe("Start Kvis")
+
+    expect(editButton).toBeTruthy();
+    expect(editButton.textContent).toBe("Edit Kvis")
+})
+
+it('should assign properties correctly', () => {
+    const kvis = screen.getByTestId("kvislistelement-test-kvisname");
+    const questions = screen.getByTestId("kvislistelement-test-questions");
+
+    expect(kvis).toBeTruthy();
+    expect(kvis.textContent).toBe(mockKvis.name)
+
+    expect(questions).toBeTruthy();
+    expect(questions.textContent).toBe(mockKvis.questions.length + " Questions")
+    expect(questions).toHaveStyle({margin: 0})
+});
