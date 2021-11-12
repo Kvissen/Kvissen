1203a50c1aab1f6d8ebb970bcd3cfe6a16f35f25
Logstor <a.rottger_rydahl@live.dk>
2021-11-12 14:47:30 +0100

    Updated dev url, and added www. Furthermore removed wrong 8080 port

--- src/main/java/common/EnvVars.java
+++ src/main/java/common/EnvVars.java
@@ -18,10 +18,10 @@ public class EnvVars {
     public static String getBaseUrl() {
         switch (DEPLOYMENT_TYPE.toLowerCase()) {
             case "prod":
-                return "http://www.kvissen.devops.diplomportal.dk:8080";
+                return "http://www.kvissen.devops.diplomportal.dk";
             case "dev":
                 // Debug server
-                return "https://kvissen-dev.caprover-root.kvissen.devops.diplomportal.dk";
+                return "https://www.kvissen-dev.caprover.kvissen.devops.diplomportal.dk";
             default:
                 // On your PC :-)
                 return "http://localhost:8080";
