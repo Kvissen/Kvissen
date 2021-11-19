740811e26722c79be508f12ca8fd02a7def7d518
erlendtyrmi <s132962@dtu.dk>
2021-11-19 01:23:38 +0100

    login and out of admin/creator role
    Signed-off-by: erlendtyrmi <s132962@dtu.dk>

--- ReactApp/src/components/header/DropDownMenu.tsx
+++ ReactApp/src/components/header/DropDownMenu.tsx
@@ -56,7 +56,7 @@ export default function DropDownMenu() {
                                     history.push("/landing")
                                 }
                             }
-                            endIcon={<LoginIcon/>}>Log out</Button>
+                        >View & Edit</Button>
                     </MenuItem> : null
                 }
                 {isLoggedInAs("creator") ?
